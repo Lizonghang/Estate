0c36f6cde34242ac079e11da0ab1f15a2c4e3cac
lizonghang <870644199@qq.com>
2017-05-03 00:37:47 +0800

    部署前端项目

--- Estate/urls.py
+++ Estate/urls.py
@@ -8,5 +8,5 @@ handler500 = 'WeWeb.views.handler500'
 urlpatterns = [
     url(r'^backend/xadmin/', include(xadmin.site.urls)),
     url(r'^api/weweb/', include('WeWeb.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^source/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 ]
