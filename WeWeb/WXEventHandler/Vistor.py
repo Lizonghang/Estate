e59c632c3704fe779b88a0f5f3ea59c18358ff60
lizonghang <870644199@qq.com>
2017-05-04 11:15:38 +0800

    访客预约

--- WeWeb/WXEventHandler/Vistor.py
+++ WeWeb/WXEventHandler/Vistor.py
@@ -1,6 +1,8 @@
 # coding=utf-8
+from django.core.exceptions import ObjectDoesNotExist
 from WeWeb.models import *
 import time
+import re
 
 
 class VisitorHandler:
@@ -11,16 +13,19 @@ class VisitorHandler:
     def get_message(self):
         fromUser, toUser = self._parse(self.XMLdata)
         user = User.objects.get(username=fromUser)
+        try:
+            user.userinfo
+        except ObjectDoesNotExist:
+            return self._get_fail_response(fromUser, toUser)
         Visitor.objects.create(master=user)
-        ret_xml = self._get_subscribe_response(fromUser, toUser)
-        return ret_xml
+        return self._get_success_response(fromUser, toUser)
 
     def _parse(self, XMLdata):
         fromUser = XMLdata.find('FromUserName').get_text()
         toUser = XMLdata.find('ToUserName').get_text()
         return fromUser, toUser
 
-    def _get_subscribe_response(self, fromUser, toUser):
+    def _get_success_response(self, fromUser, toUser):
         create_time = int(time.time())
         ret_xml = """
         <xml>
@@ -28,6 +33,75 @@ class VisitorHandler:
          <FromUserName><![CDATA[""" + toUser + """]]></FromUserName>
          <CreateTime>""" + str(create_time) + """</CreateTime>
          <MsgType><![CDATA[text]]></MsgType>
-         <Content><![CDATA[请输入来访客人姓名]]></Content>
+         <Content><![CDATA[请输入来访客人姓名,如#张三#。若有多人来访,仅填写一人姓名即可。]]></Content>
+        </xml> """
+        return ret_xml
+
+    def _get_fail_response(self, fromUser, toUser):
+        create_time = int(time.time())
+        bind = config.authorize_uri % 'url=http://www.desckie.com/#/'.replace('#', '')
+        ret_xml = """
+        <xml>
+         <ToUserName><![CDATA[""" + fromUser + """]]></ToUserName>
+         <FromUserName><![CDATA[""" + toUser + """]]></FromUserName>
+         <CreateTime>""" + str(create_time) + """</CreateTime>
+         <MsgType><![CDATA[text]]></MsgType>
+         <Content><![CDATA[您尚未进行住户信息绑定,无法进行该操作。请先进行<a href="{0}">住户绑定</a>]]></Content>
+        </xml> """.format(bind)
+        return ret_xml
+
+
+class VisitorBindHandler:
+
+    def __init__(self, XMLdata):
+        self.XMLdata = XMLdata
+
+    def get_message(self):
+        fromUser, toUser, Content = self._parse(self.XMLdata)
+        matches = re.findall(r'#(.*?)#', Content)
+        if len(matches) != 1:
+            return self._get_fail_response(fromUser, toUser, 1)
+        user = User.objects.get(username=fromUser)
+        try:
+            visitor = Visitor.objects.get(master=user)
+        except ObjectDoesNotExist:
+            return self._get_fail_response(fromUser, toUser, 2)
+        ret_xml = self._get_success_response(fromUser, toUser, visitor)
+        return ret_xml
+
+    def _parse(self, XMLdata):
+        fromUser = XMLdata.find('FromUserName').get_text()
+        toUser = XMLdata.find('ToUserName').get_text()
+        Content = XMLdata.find('Content').get_text().strip()
+        return fromUser, toUser, Content
+
+    def _get_success_response(self, fromUser, toUser, visitor):
+        msg = '预约来访客人姓名: {0}, 预约码: {1}。来访客人进入小区时请出示该预约码与个人证件,感谢您的预约!'.format(visitor.visitor, visitor.code)
+        create_time = int(time.time())
+        ret_xml = """
+        <xml>
+         <ToUserName><![CDATA[""" + fromUser + """]]></ToUserName>
+         <FromUserName><![CDATA[""" + toUser + """]]></FromUserName>
+         <CreateTime>""" + str(create_time) + """</CreateTime>
+         <MsgType><![CDATA[text]]></MsgType>
+         <Content><![CDATA[""" + msg + """]]></Content>
+        </xml> """
+        return ret_xml
+
+    def _get_fail_response(self, fromUser, toUser, errcode):
+        if errcode == 1:
+            errmsg = '请将来访客人姓名使用双#号括起,如#张三#。若有多人一起来访,仅填写其中一人姓名即可。'
+        elif errcode == 2:
+            errmsg = '请先通过"快捷服务->访客预约"创建预约记录'
+        else:
+            errmsg = '未知错误'
+        create_time = int(time.time())
+        ret_xml = """
+        <xml>
+         <ToUserName><![CDATA[""" + fromUser + """]]></ToUserName>
+         <FromUserName><![CDATA[""" + toUser + """]]></FromUserName>
+         <CreateTime>""" + str(create_time) + """</CreateTime>
+         <MsgType><![CDATA[text]]></MsgType>
+         <Content><![CDATA[""" + errmsg + """]]></Content>
         </xml> """
         return ret_xml
