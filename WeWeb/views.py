3876096ba674f4734daf190d168b981f08cd9e7e
lizonghang <870644199@qq.com>
2017-05-09 23:11:17 +0800

    FILL BUG

--- WeWeb/views.py
+++ WeWeb/views.py
@@ -119,8 +119,11 @@ def activities(request):
 def join(request):
     name = request.POST.get('name')
     user = request.user
-    activity = Activity.objects.get(name=name)
-    print activity
+    print name
+    try:
+        activity = Activity.objects.get(name=name)
+    except Exception, msg:
+        print msg
     log, new = JoinUser.objects.get_or_create(user=user, activity=activity)
     if not new:
         return JsonResponse({'err': 1, 'msg': '您已报名该活动'})
